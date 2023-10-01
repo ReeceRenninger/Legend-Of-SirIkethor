b56203152844c34d63a55ae4b90458b1ad3ce810
ReeceRenninger <renningerreece@gmail.com>
2023-10-01 11:05:37 -0700

    created helper import folder fucntion to iterate through the path file for the grass elements to be added to map

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 # need to create a file to help with importing the csv files to be useable
 from csv import reader # allows use to read csv files
 from os import walk # allows us to walk through a folder
-
+import pygame
 
 def import_csv_layout(path):
   terrain_map = []
@@ -13,9 +13,14 @@ def import_csv_layout(path):
     return terrain_map
 
 def import_folder(path):
+    surface_list = [] # create a list to store the surfaces
    # iterate through folder to import all the grass or objects or w/e we need 
-    for data in walk(path):
-       print(data) 
+    for _,__,img_files in walk(path): # walk through the path and get the image files, the _ and __ are for the root and directories which we don't need to name, saw this in a tutorial
+        for image in img_files: # iterate through the image files
+            full_path = path + '/' + image # create the full path to the image to easily import it
+            image_surf = pygame.image.load(full_path).convert_alpha() # load the image and convert it to alpha from the docs
+            surface_list.append(image_surf)
+    return surface_list
 
-import_folder('assets/graphics/grass') # test to see if the folder is being read properly
+# import_folder('assets/graphics/grass') # test to see if the folder is being read properly
 # print(import_csv_layout('assets/map/map_FloorBlocks.csv')) # test to see if the csv file is being read properly
